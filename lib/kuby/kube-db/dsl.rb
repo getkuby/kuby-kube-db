09dee3d57a4bac5d4fae642d55432ead4a81c113
Cameron Dutro <camertron@gmail.com>
2021-08-24 13:39:04 -0700

    Fix API namespace reference in autoload manifest

--- lib/kuby/kube-db/dsl.rb
+++ lib/kuby/kube-db/dsl.rb
@@ -2,7 +2,7 @@ module Kuby::KubeDB::DSL
   autoload :Catalog, 'kuby/kube-db/dsl/catalog'
   autoload :Kubedb, 'kuby/kube-db/dsl/kubedb'
   autoload :Authorization, 'kuby/kube-db/dsl/authorization'
-  autoload :Api, 'kuby/kube-db/dsl/api'
+  autoload :API, 'kuby/kube-db/dsl/api'
   autoload :Meta, 'kuby/kube-db/dsl/meta'
   autoload :V1, 'kuby/kube-db/dsl/v1'
   autoload :Appcatalog, 'kuby/kube-db/dsl/appcatalog'
--- lib/kuby/kube-db/version.rb
+++ lib/kuby/kube-db/version.rb
@@ -1,6 +1,6 @@
 module Kuby
   module KubeDB
-    VERSION = '0.7.0'.freeze
+    VERSION = '0.7.1'.freeze
     KUBEDB_VERSION = '0.12.0'.freeze
   end
 end
